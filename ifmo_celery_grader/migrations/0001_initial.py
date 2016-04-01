67cb175cd428a3f255e2c5fcb188476ff6159bfb
Dmitry Ivanyushin <defance@gmail.com>
2016-04-01 19:08:32 +0300

    Миграции для Django 1.8

--- ifmo_celery_grader/migrations/0001_initial.py
+++ ifmo_celery_grader/migrations/0001_initial.py
@@ -1,42 +1,34 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'GraderTask'
-        db.create_table('ifmo_celery_grader_gradertask', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('task_id', self.gf('django.db.models.fields.CharField')(max_length=255, db_index=True)),
-            ('task_input', self.gf('django.db.models.fields.TextField')(null=True)),
-            ('task_output', self.gf('django.db.models.fields.TextField')(null=True)),
-            ('task_state', self.gf('django.db.models.fields.CharField')(max_length=50, null=True, db_index=True)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, null=True, blank=True)),
-            ('updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal('ifmo_celery_grader', ['GraderTask'])
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'GraderTask'
-        db.delete_table('ifmo_celery_grader_gradertask')
-
-
-    models = {
-        'ifmo_celery_grader.gradertask': {
-            'Meta': {'object_name': 'GraderTask'},
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'}),
-            'task_input': ('django.db.models.fields.TextField', [], {'null': 'True'}),
-            'task_output': ('django.db.models.fields.TextField', [], {'null': 'True'}),
-            'task_state': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'db_index': 'True'}),
-            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['ifmo_celery_grader']
+    operations = [
+        migrations.CreateModel(
+            name='GraderTask',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('task_id', models.CharField(max_length=255, db_index=True)),
+                ('student_input', models.TextField(null=True)),
+                ('grader_payload', models.TextField(null=True)),
+                ('system_payload', models.TextField(null=True)),
+                ('task_input', models.TextField(null=True)),
+                ('task_output', models.TextField(null=True)),
+                ('course_id', models.CharField(max_length=255, null=True, db_index=True)),
+                ('module_id', models.CharField(max_length=255, null=True, db_index=True)),
+                ('task_type', models.CharField(max_length=50, null=True, db_index=True)),
+                ('task_state', models.CharField(max_length=50, null=True, db_index=True)),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('user_target', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+        ),
+    ]
